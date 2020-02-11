aa52e15275d5c11150db5f510dad25017ac6ee13
mh10445 <hussein@blockgemini.com>
2020-02-11 14:52:11 +0400

    first commit

--- rxjs.js
+++ rxjs.js
@@ -1,64 +1,83 @@
-class Timer {
-  constructor(startBtn, pauseBtn, durationInput, interval, events) {
-    this.startBtn = startBtn;
-    this.pauseBtn = pauseBtn;
-    this.durationInput = durationInput;
-    this.interval = interval;
-    if (events) {
-      this.onStart = events.onStart;
-      this.onComplete = events.onComplete;
-      this.onPause = events.onPause;
-      this.onTick = events.onTick;
+let apiKey = "http://www.omdbapi.com/?i=tt3896198&apikey=20cf6cfe";
+const input = document.querySelectorAll("input")[0];
+const input2 = document.querySelectorAll("input")[1];
+let collapsible = document.querySelector(".collapsible");
+
+window.addEventListener("load", function() {
+  collapsible.style.display = "none";
+});
+
+function debounce(callback, delay = 1000) {
+  let timeout;
+  return function(...args) {
+    if (timeout) {
+      clearTimeout(timeout);
     }
-    this.startBtn.addEventListener("click", this.start);
-    this.pauseBtn.addEventListener("click", this.pause);
-  }
-
-  get durationValue() {
-    return parseFloat(this.durationInput.value);
-  }
-
-  set durationValue(value) {
-    this.durationInput.value = value.toFixed(2);
-  }
-
-  start = () => {
-    if (this.onStart) {
-      this.onStart();
-    }
-    this.tick();
-    this.timer = setInterval(() => {
-      this.tick();
-    }, this.interval);
+    timeout = setTimeout(() => {
+      callback.apply(null, args);
+    }, delay);
   };
+}
 
-  pause = () => {
-    clearInterval(this.timer);
-  };
-
-  tick() {
-    if (this.durationValue <= 0) {
-      this.pause();
-      this.durationInput.value = 30;
-    } else {
-      this.durationValue = this.durationValue - 0.05;
-      if (this.onTick) {
-        this.onTick();
-      }
+async function getMovies(url, value) {
+  let response = await axios.get(url, {
+    params: {
+      s: value
     }
+  });
+  if (response.data.Errors) {
+    return [];
+  } else {
+    return response.data;
   }
 }
 
-const start = document.getElementById("start");
-const pause = document.getElementById("pause");
-const duration = document.getElementById("duration");
-const circle = document.querySelector("circle");
-let currentOffset = 0;
-const timer = new Timer(start, pause, duration, 50, {
-  onStart() {},
-  onTick() {
-    circle.setAttribute("stroke-dashoffset", currentOffset);
-    currentOffset = currentOffset - 0.05;
-  },
-  onComplete() {}
+function render(item, current) {
+  return `
+    <li>
+      <div class="collapsible-header" style="padding: 0 1rem; ${
+        current === 0
+          ? "background-color: #3f51b5 !important; color: #fff !important;transform: scale(1.05)"
+          : ""
+      }">
+        <p>${item.Title}</p>
+      </div>
+      <div class="collapsible-body">
+        <img src="${
+          item.Poster === "N/A"
+            ? "https://images.unsplash.com/photo-1581405919332-d9f2f192dc6e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+            : item.Poster
+        }" width="30%" />
+      </div>
+    </li>
+  `;
+}
+
+function headerClick() {
+  let headers = document.querySelectorAll(".collapsible-header");
+  for (let header of headers) {
+    header.addEventListener("click", function headerClick(e) {
+      input.value = e.target.textContent.trim();
+    });
+  }
+}
+
+const onInput = debounce(async function oninput(e) {
+  let data = await getMovies(apiKey, e.target.value);
+  for (let [index, item] of data.Search.entries()) {
+    let target = document.getElementById("target");
+    target.innerHTML += render(item, index);
+  }
+  headerClick();
+}, 1000);
+
+document.addEventListener("click", e => {
+  let root = document.querySelector(".dropdown");
+  if (!root.contains(e.target)) {
+    collapsible.style.display = "none";
+  } else {
+    collapsible.style.display = "block";
+  }
 });
+
+input.addEventListener("input", onInput);
